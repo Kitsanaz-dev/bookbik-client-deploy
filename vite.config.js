cbd759feaba98c8ebd37cc2b8d06e725f25a7519
Kitsanaz-Dev <kitsanaz.ctbd@gmail.com>
2026-03-12 23:16:54 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
   preview: {
     host: true,              // same as 0.0.0.0
